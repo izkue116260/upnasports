b70b99382d06503290caf3ed2d6929f3517a2627
Ainhoa <ainhoaizkue19@gmail.com>
2020-09-01 16:40:24 +0200

    Usa efectos para el menu movil

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -2,13 +2,9 @@ function abreMenu() {
     var height = 64*5;
     var menuMovil = $(".menu-movil")
     menuMovil.height(height);
-    cerrarHome()
+    menuMovil.toggleClass("clickado");
 }
 
-function cerrarHome() {
-    var menuMovil = $(".menu-movil")
-    menuMovil.toggleClass("u-display-none")
-}
 
 $(window).on("load", function () {
     if(window.location.href === "http://localhost:3005/proyecto-final/home/") {
